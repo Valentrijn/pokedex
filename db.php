836a56af4c994e011263fc3ab7d704083a5870fb
tijnv <tijnvalen6@gmail.com>
2025-05-14 11:32:20 +0200

    - wachtwoord veranderd - title veranderd

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$user = 'root';
+$user = '200687';
 $pass = '';
 $host = 'localhost';
 $db = 'pokedex';
